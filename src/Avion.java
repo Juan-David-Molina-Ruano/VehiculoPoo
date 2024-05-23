4527bf3f25515ccd02099c5ff69352b554ffdaf9
Nestor.Adonay.Rodriguez.Alberto@gmail.com <nestor.adonay.rodriguez.alberto@gmail.com>
2024-05-22 20:55:03 -0600

    Arreglos

--- src/Avion.java
+++ src/Avion.java
@@ -1,3 +1,24 @@
-public class Avion {
+public class Avion extends Vehiculo
+{
+    public Avion()
+    {
+        
+    }
+    public Avion(String nombre, String color, Double precio)
+    {
+        super(nombre,color,precio);
+    }
 
+    @Override
+    public void mostrarInfo() 
+    {
+       System.out.println("_ _ _ Datos Del Avion _ _ _");
+       System.out.println("NOMBRE: "+getNombre()+" COLOR: "+getColor()+" PRECIO: "+getPrecio());
+    }
+
+    @Override
+    public void mostrarVehiculo()
+    {
+        System.out.println("METODO SOBRE ESCRITO EN Avion");
+    }
 }
